1074cfab898c3857cae008785ebd9e2bdfb1785c
Fabio <fabiochirinos7@gmail.com>
2022-11-07 17:40:19 -0500

    se modifico el estilo de la clase fila

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import logo from './img/calculadora.jpg'
+import logo from './img/calculadora.png'
 
 function App() {
   return (
     <div className='App'>
       <div className='logo-contenedor'>
-        <img 
-          src={logo}
-          className='logo'
-          alt='Logo de calculadora' />
+        <h1>Calculadora 🧮</h1>
+      </div>
+      <div className='contenedor-calculadora'>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
+        <div className='fila'></div>
       </div>
     </div>
   );
