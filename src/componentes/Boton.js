bacede2f76fdb57da55f7a9a18f58541bcb87593
Fabio <fabiochirinos7@gmail.com>
2022-11-09 10:10:36 -0500

    se agrego el evento onClic

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -4,13 +4,14 @@ import '../hojas-de-estilo/Boton.css'
 export default function Boton(props) {
 
   const esOperador = valor => {
-    return isNaN(valor) && (valor != '.') && (valor !=  '=');
+    return isNaN(valor) && (valor != '.') && (valor != '=');
   };
 
 
   return (
     <div
-      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}>
+      className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
+      onClick={() => props.manejarClic(props.children)} >
       {props.children}
     </div>
   )
