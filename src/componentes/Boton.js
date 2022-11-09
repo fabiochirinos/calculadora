8f551c42c4ad394b1d699c0af551004812c0527a
Fabio <fabiochirinos7@gmail.com>
2022-11-09 18:53:53 -0500

    se modifico el componente Boton

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -4,15 +4,15 @@ import '../hojas-de-estilo/Boton.css'
 export default function Boton(props) {
 
   const esOperador = valor => {
-    return isNaN(valor) && (valor != '.') && (valor != '=');
+    return isNaN(valor) && (valor !== '.') && (valor !== '=');
   };
 
 
   return (
-    <div
+    <button
       className={`boton-contenedor ${esOperador(props.children) ? 'operador' : ''}`.trimEnd()}
       onClick={() => props.manejarClic(props.children)} >
       {props.children}
-    </div>
+    </button>
   )
 }
